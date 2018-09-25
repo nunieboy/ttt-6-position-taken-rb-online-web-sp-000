e130656a44f9afdb958ef914d6f03f3a2166c2f0
Pablo Eusebio <nunieboy88@gmail.com>
2018-09-25 16:03:25 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,4 @@
 # code your #position_taken? method here!
 
-def position_taken(position)
-  if board[position] == "X"
-    puts "position already taken"
-  else
-    board[position] = "X"
-  end
+def position_taken(position, index)
 end
